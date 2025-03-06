686c610b011f96d2ee993d23bc7fb53ffadbbc2d
default <yunus.emre.guzel@mths.ca>
2025-03-06 18:29:47 +0000

    added echo and changed the width

--- answer.php
+++ answer.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-  <html>
-  <head>
+<html>
+
+<head>
   <meta charset="utf-8" />
   <meta name="description" content="Hello world in JS " />
   <meta name="keywords" content="mths, icd2o" />
@@ -19,8 +20,9 @@
   <link rel="stylesheet" href="css/style.css">
   <title>Hello world in js </title>
 </head>
-  <body>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
@@ -30,12 +32,17 @@
     <main class="mdl-layout__content">
       <div class="right-image">
       </div>
-      <div class="page-content">Hello, World in php</div>
+      <div class="page-content">
+        <?php
+        echo "Hello,World!";
+        ?>
+      </div>
       <br />
       <img src="images/Hello_World_Brian_Kernighan_1978.jpg" alt="">
       <div class="page-content-answer">
         <a href="./index.php">Return ...</a>
       </div>
     </main>
-  </body>
+</body>
+
 </html>
